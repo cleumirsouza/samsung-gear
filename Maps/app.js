4773e680da5264cc9bc2ca5b49e920e8a6599db2
Cleumir Souza <cleumir.s@samsung.com>
2016-05-09 15:04:43 -0400

    add maps and marker point in the map
    Signed-off-by: Cleumir Souza <cleumir.s@samsung.com>

--- Maps/app.js
+++ Maps/app.js
@@ -1,16 +1,21 @@
-( function () {
-	window.addEventListener( 'tizenhwkey', function( ev ) {
-		if( ev.keyName === "back" ) {
-			var page = document.getElementsByClassName( 'ui-page-active' )[0],
-				pageid = page ? page.id : "";
-			if( pageid === "main" ) {
-				try {
-					tizen.application.getCurrentApplication().exit();
-				} catch (ignore) {
-				}
-			} else {
-				window.history.back();
-			}
-		}
-	} );
-} () );
+function initMap() {
+
+	var myLatLng = {
+		lat: -3.1342116,
+		lng: -59.9805424
+	};
+
+	// Create a map object and specify the DOM element for display.
+	var map = new google.maps.Map(document.getElementById('map'), {
+		center: myLatLng,
+		disableDefaultUI: true,
+	    zoom: 14
+	});
+	
+	//create Marker
+	var marker = new google.maps.Marker({
+	  map: map,
+	  position: myLatLng,
+	  title: 'SIDIA!'
+	});
+}
